264cbf9b0a73d88d1b48e4098daff74dc9d76e40
xuyongshi <heliocc@163.com>
2023-05-12 00:44:36 +0800

    add module

--- enhance-agent-core/src/main/java/com/example/demo/EnhanceAgentPremain.java
+++ enhance-agent-core/src/main/java/com/example/demo/EnhanceAgentPremain.java
@@ -1,4 +1,19 @@
 package com.example.demo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.instrument.Instrumentation;
+
 public class EnhanceAgentPremain {
+
+    private final static Logger log = LoggerFactory.getLogger(EnhanceAgentPremain.class);
+
+    public static void premain(String agentArgs, Instrumentation instrumentation) throws Throwable {
+        log.info("Enhance agent premain start...");
+        log.info("agentArgs={}, instrumentation={}", agentArgs, instrumentation);
+        log.info("Enhance agent premain end...");
+    }
+
 }
